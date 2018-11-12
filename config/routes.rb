aa0d7295f4201678e3fd561e8b3973cf26810cb9
Jamie Brouwer <jamesonbrouwer@gmail.com>
2018-11-12 14:06:35 +0100

    asking questions between pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'questions/ask', to: "questions#ask"
+  post 'questions/ask', to: 'questions#answer', as: :post
+  get 'questions/answer', to: "questions#answer", as: :answer
+  # get 'questions/answer', to: "questions#ask"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
